f7e1fed3a2cc32e817fe5aad2a77273cfc7b5d0d
muskan pandey <pandey03muskan@gmail.com>
2023-11-04 17:32:38 +0530

    first commit

--- collegeproject/src/App.js
+++ collegeproject/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
+npm i --save @fortawesome/fontawesome-svg-core
+npm i --save @fortawesome/free-solid-svg-icons
+npm i --save @fortawesome/free-regular-svg-icons
+npm i --save @fortawesome/free-brands-svg-icons
 import './App.css';
+import Outer from './component/OuterHeaderSlider'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Outer></Outer>
     </div>
   );
 }
---
+++ collegeproject/src/component/OuterHeaderSlider.js
@@ -0,0 +1,11 @@
+import './OuterHeaderSlider.css'
+function Outer(){
+    return(
+        <div className="outer">
+            <header>
+                <div class="upperHeader"></div>
+            </header>
+        </div>
+    );
+}
+export default Outer;
